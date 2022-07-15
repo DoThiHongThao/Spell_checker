f8dfac4ec349187ef6fcde1b97be966b5a2a5a11
Le Bui <lebui@Les-MacBook-Air.local>
2022-07-15 16:23:36 +0700

    up code

--- src/pages/DownloadFile.tsx
+++ src/pages/DownloadFile.tsx
@@ -5,7 +5,8 @@ function DownloadFile() {
   const handleDownload = () => {
     const pdfApi = new PdfApi(
       '6dda0656-214b-451c-8443-db7b09565a78',
-      'bd4b574a70a98b27163a435a8c4e98da'
+      'bd4b574a70a98b27163a435a8c4e98da',
+      'https://api.aspose.cloud/v3.0'
     );
 
     pdfApi
